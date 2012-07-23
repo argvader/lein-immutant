819fc88c4dd4c3b67e36ed7a2c80ee1160b4e5d4
argvader <gary@theoryblister.com>
2012-07-23 09:57:50 -0500

    Update src/leiningen/immutant/run.clj

--- src/leiningen/immutant/run.clj
+++ src/leiningen/immutant/run.clj
@@ -5,17 +5,20 @@
 
 (let [jboss-home (common/get-jboss-home)]
   (defn standalone-sh []
-    (str (.getAbsolutePath jboss-home) "/bin/standalone.sh"))
+    (let [suffix (if (= (.. System (getProperties) (get "os.name") (toLowerCase) (indexOf "win")) 0)
+      (str "bat")
+      (str "sh"))]
+      (str (.getAbsolutePath jboss-home) "/bin/standalone." suffix)))
 
-  (defn run
-    "Starts up the Immutant specified by ~/.lein/immutant/current or $IMMUTANT_HOME, displaying its console output"
-    ([]
-       (run nil))
-    ([project & opts]
-       (util/with-jboss-home jboss-home
-         (and project (not (util/application-is-deployed? project nil))
-              (common/err "WARNING: The current app is not deployed - deploy with 'lein immutant deploy'"))
-         (let [script (standalone-sh)
-               params (replace {"--clustered" "--server-config=standalone-ha.xml"} opts)]
-           (apply println "Starting Immutant:" script params)
-           (apply shim/lein-sh-fn script params))))))
+(defn run
+  "Starts up the Immutant specified by ~/.lein/immutant/current or $IMMUTANT_HOME, displaying its console output"
+  ([]
+    (run nil))
+  ([project & opts]
+    (util/with-jboss-home jboss-home
+      (and project (not (util/application-is-deployed? project nil))
+        (common/err "WARNING: The current app is not deployed - deploy with 'lein immutant deploy'"))
+      (let [script (standalone-sh)
+            params (replace {"--clustered" "--server-config=standalone-ha.xml"} opts)]
+        (apply println "Starting Immutant:" script params)
+        (apply shim/lein-sh-fn script params))))))
